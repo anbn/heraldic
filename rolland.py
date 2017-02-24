ec0b9c87ad2e68d3c5cef0d95974fa1a0c5a3758
ab <ab@elon>
2017-02-24 14:47:23 +0100

    harris corner detection

--- rolland.py
+++ rolland.py
@@ -11,6 +11,7 @@ from skimage.exposure import equalize_hist, histogram, cumulative_distribution
 from skimage.transform import hough_line, hough_line_peaks, probabilistic_hough_line
 from skimage.feature import canny
 
+from skimage.feature import corner_harris, corner_subpix, corner_peaks
 
 
 
@@ -36,36 +37,13 @@ if __name__ == "__main__":
         print "%d/%d %s" % (n+1,len(file_list),f)
 
         imi = io.imread(f, as_grey=True)
-        imi = imi[:,imi.shape[1]*0.6:]
-        imi = canny(imi)
-        h, theta, d = hough_line(imi)
+        image = imi[:,imi.shape[1]*0.6:]
+        
+        coords = corner_peaks(corner_harris(image), min_distance=5)
+        coords_subpix = corner_subpix(image, coords, window_size=13)
 
-        fig, axes = plt.subplots(1, 3, figsize=(15, 6), subplot_kw={'adjustable': 'box-forced'})
-        ax = axes.ravel()
-
-        ax[0].imshow(imi, cmap="gray")
-        ax[0].set_title('Input image')
-        ax[0].set_axis_off()
-
-        ax[1].imshow(np.log(1 + h), extent=[np.rad2deg(theta[-1]), np.rad2deg(theta[0]), d[-1], d[0]],
-                                          cmap="gray", aspect=1/1.5)
-        ax[1].set_title('Hough transform')
-        ax[1].set_xlabel('Angles (degrees)')
-        ax[1].set_ylabel('Distance (pixels)')
-        ax[1].axis('image')
-
-        ax[2].imshow(imi, cmap="gray")
-        for _, angle, dist in zip(*hough_line_peaks(h, theta, d, threshold=0.0*np.max(h), min_distance=300)):
-            y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
-            y1 = (dist - imi.shape[1] * np.cos(angle)) / np.sin(angle)
-            ax[2].plot((0, imi.shape[1]), (y0, y1), '-r')
-            ax[2].set_xlim((0, imi.shape[1]))
-            ax[2].set_ylim((imi.shape[0], 0))
-            ax[2].set_axis_off()
-            ax[2].set_title('Detected lines')
-
+        fig, ax = plt.subplots()
+        ax.imshow(image, interpolation='nearest', cmap=plt.cm.gray)
+        ax.plot(coords[:, 1], coords[:, 0], '.b', markersize=3)
+        ax.plot(coords_subpix[:, 1], coords_subpix[:, 0], '+r', markersize=15)
         plt.show()
-
-        if True:
-            plt.figure("image in"), plt.imshow(imi, cmap="gray")
-            plt.show()
